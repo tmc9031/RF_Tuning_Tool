abcaa6b7fb0ca33315bcd941ce8d4d26eef3d93b
maomaoto_office <maomaoto+office@gmail.com>
2014-05-29 14:04:02 +0800

    Add PS_GW_PPT1830.py for power supply Good Will PPT-1830

---
+++ PS_GW_PPT1830.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+"""
+	For power supply GW Instek PPT1830 GPIB commands
+	Using PyVisa and Python 2.7.5
+	
+	 This file is part of RF_Tuning_Tool.
+
+	:copyright: (c) 2013 by the A-mao Chang (maomaoto@gmail.com)
+	:license: MIT, see COPYING for more details.
+
+"""
+
+from visa import *
+import time
+from decimal import *
+from WCDMA_attributes import *
+
+class PS_GW_PPT1830(Instrument):
+	"""
+		class for PS_GW_PPT1830
+	"""
+	def __init__(self, resource_name, **keyw):
+		super(PS_GW_PPT1830, self).__init__(resource_name, **keyw)
+	
+	def __repr__(self):
+		return "PS_GW_PPT1830({0})".format(self.resource_name)
+		
+	def identity(self):
+		"""
+			return instrument name
+		"""
+		s = self.ask("*IDN?")
+		return s
+		
+	def read_current(self):
+		"""
+			return output current in float
+		"""
+		s = self.ask("MEASure:CURRent?")
+		return float(s)
+	
+		
+if __name__ == "__main__":
+	
+	power_supply = PS_GW_PPT1830("GPIB::8")
+	power_supply.timeout = 10
+	print("*IDN?")
+	print(power_supply.ask("*IDN?"))
+	
+	print(power_supply.identity())
+	
+	print("current:{0}".format(power_supply.read_current()))
+	
+	
+	
